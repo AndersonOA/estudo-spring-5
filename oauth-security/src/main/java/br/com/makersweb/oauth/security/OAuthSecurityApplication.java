af8b8d9b375120ca505e86210af678214e3f8dfc
Anderson Orione Aristies <anderson.aristides@fh.com.br>
2018-03-28 23:35:17 -0300

    Ajuste

--- oauth-security/src/main/java/br/com/makersweb/oauth/security/OAuthSecurityApplication.java
+++ oauth-security/src/main/java/br/com/makersweb/oauth/security/OAuthSecurityApplication.java
@@ -2,8 +2,9 @@ package br.com.makersweb.oauth.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class OAuthSecurityApplication {
 
 	public static void main(String[] args) {
